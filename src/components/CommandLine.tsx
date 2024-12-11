4233347c8dabaaa032fe252bd63848902d83a670
Doston Nabotov <dostonnabotov@gmail.com>
2024-12-11 09:01:09 +0200

    Added helper message when starting

--- src/components/CommandLine.tsx
+++ src/components/CommandLine.tsx
@@ -6,12 +6,15 @@ type Props = {
   handleCommand: (cmd: string) => void;
 };
 
+let showHelperMessage = true;
+
 const CommandLine = ({ command, setCommand, handleCommand }: Props) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     handleCommand(command);
+    showHelperMessage = false;
 
     // keep the input in focus after command submission
     if (inputRef.current) {
@@ -29,6 +32,7 @@ const CommandLine = ({ command, setCommand, handleCommand }: Props) => {
           value={command}
           onChange={(e) => setCommand((e.target as HTMLInputElement).value)}
           className="terminal__input"
+          placeholder={showHelperMessage ? "Type 'help' to get started" : ""} 
         />
       </form>
     </div>
